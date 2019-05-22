71b445e088a96e60a6ae4cebddb37f32e0ec475f
Becki Wordsworth <r.a.wordsworth@gmail.com>
2019-05-22 23:02:04 +0200

    Set-up  of the app

---
+++ react-hooks/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
